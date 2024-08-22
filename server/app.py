aa339a9c1204b0ec9b25ca32796caf440eab1dbf
Chugaister <dmytroshvahyliak@gmail.com>
2024-08-22 15:14:23 +0300

    docker set up

--- server/app.py
+++ server/app.py
@@ -2,19 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
-import core.exceptions.base
 from api import router
 from core.middlewares.middlewares import XProcessTimeMiddleware
-# from core.middlewares.exchandlers import HTTPErrorExceptionHandler
 
 
 def init_routers(app_: FastAPI) -> None:
     app_.include_router(router, prefix="/api")
 
-
-# def init_exchandlers(app_: FastAPI) -> None:
-#     app_.add_exception_handler(core.exceptions.base.HTTPError, HTTPErrorExceptionHandler.handle)
-#
 
 def init_middlewares(app_: FastAPI) -> None:
     app_.add_middleware(XProcessTimeMiddleware)
@@ -24,7 +18,7 @@ def init_middlewares(app_: FastAPI) -> None:
             "allow_origins": ["*"],
             "allow_credentials": True,
             "allow_methods": ["*"],
-            "allow_headers": ["*"],
+            "allow_headers": ["*"]
         }
     )
 
@@ -47,7 +41,6 @@ async def lifespan(app_: FastAPI):
 def create_app() -> FastAPI:
     app_ = FastAPI(lifespan=lifespan)
     init_routers(app_)
-    # init_exchandlers(app_)
     init_middlewares(app_)
     return app_
 
--- server/config.py
+++ server/config.py
@@ -10,14 +10,14 @@ class Config:
 
     def __init__(self):
         load_dotenv()
-        self.DB_URL = self.get_var("DB_URL")
-        self.HOST = self.get_var("HOST")
-        self.PORT = int(self.get_var("PORT"))
-        self.SECRET_KEY = self.get_var("SECRET_KEY")
-        self.SECRET_KEY = self.get_var("SECRET_KEY")
-        self.SSL_CERTFILE_PATH = self.get_var("SSL_CERTFILE_PATH", optional=True)
-        self.SSL_KEYFILE_PATH = self.get_var("SSL_KEYFILE_PATH", optional=True)
-        self.SSL_CA_BUNDLE_FILE_PATH = self.get_var("SSL_CA_BUNDLE_FILE_PATH", optional=True)
+        self.POSTGRES_USER = self.get_var("POSTGRES_USER")
+        self.POSTGRES_PASSWORD = self.get_var("POSTGRES_PASSWORD")
+        self.POSTGRES_HOST = self.get_var("POSTGRES_HOST")
+        self.POSTGRES_DB = self.get_var("POSTGRES_DB")
+
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:5432/{self.POSTGRES_DB}"
 
     @staticmethod
     def get_var(item: str, optional: bool = False):
